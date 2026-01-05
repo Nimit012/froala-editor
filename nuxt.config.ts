c1454142a16abfddbe2c08524bd09d387150a2bd
Nimit012 <nimit.jain@comprotechnologies.com>
2026-01-05 15:26:48 +0530

    feat: add embedly and tui plugins

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,39 @@
-import svgLoader from 'vite-svg-loader'
+import svgLoader from "vite-svg-loader";
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  srcDir: 'app',
-  plugins: [
-    '~/plugins/froala.client.ts'
-  ],
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ],
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://cdn.jsdelivr.net/npm/tui-image-editor@3.2.2/dist/tui-image-editor.css",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://uicdn.toast.com/tui-color-picker/latest/tui-color-picker.css",
+        },
+      ],
+      script: [
+        {
+          src: "https://cdnjs.cloudflare.com/ajax/libs/fabric.js/1.6.7/fabric.min.js",
+          defer: true,
+        },
+        {
+          src: "https://cdn.jsdelivr.net/npm/tui-code-snippet@1.4.0/dist/tui-code-snippet.min.js",
+          defer: true,
+        },
+        {
+          src: "https://cdn.jsdelivr.net/npm/tui-image-editor@3.2.2/dist/tui-image-editor.min.js",
+          defer: true,
+        },
+      ],
+    },
+  },
+  compatibilityDate: "2025-07-15",
+  srcDir: "app",
+  plugins: ["~/plugins/froala.client.ts"],
+  modules: ["@nuxtjs/tailwindcss"],
   devtools: { enabled: true },
   vite: {
-    plugins: [
-      svgLoader()
-    ]
-  }
-})
+    plugins: [svgLoader()],
+  },
+});
